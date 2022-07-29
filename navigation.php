b02cc8d2fd5133b45dacedbe363ec2dd1c3b5f30
oluwaCyna <oluwashinaayomi2019@gmail.com>
2022-07-29 03:06:41 -0700

    Homepage and Market page- Deskstop view

---
+++ navigation.php
@@ -0,0 +1,13 @@
+<div class="" id="header">
+    <div class="" id="logo">
+        <img src="https://i.ibb.co/gTBPJ48/logo.jpg" height="100" width="150" alt="logo" />
+    </div>
+    <nav class="" id="nav">
+        <ul>
+            <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="market.php">Market</a></li>
+            <li class="nav-item"><a class="nav-link" href="/">Login/Register</a></li>
+            <li class="nav-item"><a class="nav-link" href="">Contact</a></li>
+        </ul>
+    </nav>
+</div>
