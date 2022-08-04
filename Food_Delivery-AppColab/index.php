583c24636fff3c4b488a2bd895376314f636f0c0
Michealbori <100783269+Michealbori@users.noreply.github.com>
2022-08-04 02:53:31 -0700

    Add files via upload

---
+++ Food_Delivery-AppColab/index.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Poppins:wght@100;200;300&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="img/food-point-logo-symbol-and-icon-template-to-show-the-location-of-the-food-seller-vector.webp">
+</head>
+
+<body>
+    <header class="" id="">
+        <?php include_once "navigation.php" ?>
+
+        <div id="hero" class="">
+            <!-- <img id="hero-image" src="https://wallpaperaccess.com/full/271726.jpg" alt="Banner" /> -->
+            <div class="" id="hero-text">
+                <p>LEARN HOW TO MAKE</p>
+                <h1>Order Healthy and Fresh Food Any Time</h1>
+                <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown</p>
+                <form>
+                    <input type="text" name="search" placeholder="Search..." value="" id="search" class="input" />
+                    <button type="submit" class="btn" id="submit" name="submit">Search</button>
+                </form>
+            </div>
+    </header>
+
+    <section class="section-container" id="category">
+        <div class="container">
+            <div class="section-head-container">
+                <h6>OUR TOP CATEGORIES</h6>
+                <h2>Categories</h2>
+                <div class="line-group">
+                    <hr class="line" />
+                    <hr class="line2" />
+                    <hr class="line2" />
+                    <hr class="line2" />
+                </div>
+            </div>
+            <div class="category-list">
+                <div class="category-card" id="">
+                    <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/Layer256-1.png" height="220" width="250" alt="Category Image" />
+                    <div class="category-card-text">
+                        <h6>Category Name</h6>
+                        <p>description</p>
+                    </div>
+                </div>
+
+                <div class="category-card" id="">
+                    <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/1qLayer-1.png" height="220" width="250" alt="Category Image" />
+                    <div class="category-card-text">
+                        <h6>Category Name</h6>
+                        <p>description</p>
+                    </div>
+                </div>
+
+                <div class="category-card" id="">
+                    <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/sa1.png" height="220" width="250" alt="Category Image" />
+                    <div class="category-card-text">
+                        <h6>Category Name</h6>
+                        <p>description</p>
+                    </div>
+                </div>
+
+                <div class="category-card" id="">
+                    <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/pngwing.png" height="220" width="250" alt="Category Image" />
+                    <div class="category-card-text">
+                        <h6>Category Name</h6>
+                        <p>description</p>
+                    </div>
+                </div>
+
+                <div class="category-card" id="">
+                    <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/Layer256-1.png" height="220" width="250" alt="Category Image" />
+                    <div class="category-card-text">
+                        <h6>Category Name</h6>
+                        <p>description</p>
+                    </div>
+                </div>
+
+                <div class="category-card" id="">
+                    <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/1qLayer-1.png" height="220" width="250" alt="Category Image" />
+                    <div class="category-card-text">
+                        <h6>Category Name</h6>
+                        <p>description</p>
+                    </div>
+                </div>
+
+                <div class="category-card" id="">
+                    <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/sa1.png" height="220" width="250" alt="Category Image" />
+                    <div class="category-card-text">
+                        <h6>Category Name</h6>
+                        <p>description</p>
+                    </div>
+                </div>
+
+                <div class="category-card" id="">
+                    <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/pngwing.png" height="220" width="250" alt="Category Image" />
+                    <div class="category-card-text">
+                        <h6>Category Name</h6>
+                        <p>description</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="" id="category">
+        <div class="container">
+            <div class="section-head-container">
+                <h6>OUR TOP MEALS</h6>
+                <h2>Delecious Meals</h2>
+                <div class="line-group">
+                    <hr class="line" />
+                    <hr class="line2" />
+                    <hr class="line2" />
+                    <hr class="line2" />
+                </div>
+            </div>
+            <div class="meal-list">
+                <div class="meal-card" id="">
+                    <div class="meal-card-image">
+                        <div class="meal-card-div"></div>
+                        <div class="meal-card-div2"></div>
+                        <div class="meal-card-div3"></div>
+                        <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/Layer256-1.png" height="150" width="150" alt="Category Image" />
+                    </div>
+                    <div class="meal-card-text">
+                        <p>star rating</p>
+                        <h6>Category Name</h6>
+                        <p><s>$300</s> $700</p>
+                    </div>
+                </div>
+
+                <div class="meal-card" id="">
+                    <div class="meal-card-image">
+                        <div class="meal-card-div"></div>
+                        <div class="meal-card-div2"></div>
+                        <div class="meal-card-div3"></div>
+                        <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/Layer256-1.png" height="150" width="150" alt="Category Image" />
+                    </div>
+                    <div class="meal-card-text">
+                        <p>star rating</p>
+                        <h6>Category Name</h6>
+                        <p><s>$300</s> $700</p>
+                    </div>
+                </div>
+
+                <div class="meal-card" id="">
+                    <div class="meal-card-image">
+                        <div class="meal-card-div"></div>
+                        <div class="meal-card-div2"></div>
+                        <div class="meal-card-div3"></div>
+                        <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/Layer256-1.png" height="150" width="150" alt="Category Image" />
+                    </div>
+                    <div class="meal-card-text">
+                        <p>star rating</p>
+                        <h6>Category Name</h6>
+                        <p><s>$300</s> $700</p>
+                    </div>
+                </div>
+
+                <div class="meal-card" id="">
+                    <div class="meal-card-image">
+                        <div class="meal-card-div"></div>
+                        <div class="meal-card-div2"></div>
+                        <div class="meal-card-div3"></div>
+                        <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/Layer256-1.png" height="150" width="150" alt="Category Image" />
+                    </div>
+                    <div class="meal-card-text">
+                        <p>star rating</p>
+                        <h6>Category Name</h6>
+                        <p><s>$300</s> $700</p>
+                    </div>
+                </div>
+
+                <div class="meal-card" id="">
+                    <div class="meal-card-image">
+                        <div class="meal-card-div"></div>
+                        <div class="meal-card-div2"></div>
+                        <div class="meal-card-div3"></div>
+                        <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/Layer256-1.png" height="150" width="150" alt="Category Image" />
+                    </div>
+                    <div class="meal-card-text">
+                        <p>star rating</p>
+                        <h6>Category Name</h6>
+                        <p><s>$300</s> $700</p>
+                    </div>
+                </div>
+
+                <div class="meal-card" id="">
+                    <div class="meal-card-image">
+                        <div class="meal-card-div"></div>
+                        <div class="meal-card-div2"></div>
+                        <div class="meal-card-div3"></div>
+                        <img src="https://marketplace.foodotawp.com/wp-content/uploads/2021/05/Layer256-1.png" height="150" width="150" alt="Category Image" />
+                    </div>
+                    <div class="meal-card-text">
+                        <p>star rating</p>
+                        <h6>Category Name</h6>
+                        <p><s>$300</s> $700</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="section-container" id="team">
+        <div class="container">
+            <div class="section-head-container">
+                <h6>OUR LEADING TEAM</h6>
+                <h2>TEAM</h2>
+                <div class="line-group">
+                    <hr class="line" />
+                    <hr class="line2" />
+                    <hr class="line2" />
+                    <hr class="line2" />
+                </div>
+            </div>
+            <div class="team-list">
+                <div class="team-card" id="">
+                    <img src="https://i.ibb.co/tm0C0Gm/use-Copy.jpg" height="450" width="350" alt="Team Image" />
+                    <div class="team-card-text">
+                        <h6>Category Name</h6>
+                        <p>description</p>
+                    </div>
+                </div>
+
+
+                <div class="team-card" id="">
+                    <img src="img/f801ba69-12a5-4934-a463-d69a25500d02.jpg" height="450" width="350" alt="Team Image" />
+                    <div class="team-card-text">
+                        <h6>Category Name</h6>
+                        <p>description</p>
+                    </div>
+                </div>
+
+                <div class="team-card" id="">
+                    <img src="https://i.ibb.co/tm0C0Gm/use-Copy.jpg" height="450" width="350" alt="Team Image" />
+                    <div class="team-card-text">
+                        <h6>Category Name</h6>
+                        <p>description</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="" id="how">
+        <div class="how-list">
+            <div class="how-card" id="">
+                <i class="fa-solid fa-list how-icon"></i>
+                <h6>View Food Item</h6>
+                <p>Thank you for being valued customer. We are so grateful to serving for the honored be clients pleasure of serving hope we meets.</p>
+            </div>
+
+            <div class="how-card" id="">
+                <i class="fa-solid fa-credit-card how-icon"></i>
+                <h6>Place Order</h6>
+                <p>Online food Delivery for hiring Food Foodota We appreciate your business, and we’ll do best to continue to give you the new kind.</p>
+            </div>
+
+            <div class="how-card" id="">
+                <i class="fa-solid fa-truck how-icon"></i>
+                <h6>Deliver to Doorstep</h6>
+                <p>We at truly appreciate your business and we’re grateful for the trust you’ve placed in us. We sincerely hope you are satisfied .</p>
+            </div>
+        </div>
+        <div class="container" id="">
+            <div class="section-head-container" id="how-header">
+                <h6 class="section-title">HOW IT WORKS</h6>
+                <h2>Simple Process</h2>
+                <div class="line-group">
+                    <hr class="line" />
+                    <hr class="line2" />
+                    <hr class="line2" />
+                    <hr class="line2" />
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="container p-0" id="footer">
+            <div>
+                <h4>Main Menu</h4>
+                <p><a style=" text-decoration: none; color: white; font-weight: 700;"  href="market.php">Snacks</a> </p>
+                <p><a style=" text-decoration: none; color: white; font-weight: 700;"  href="market.php">Swallow</a> </p>
+                <p><a style=" text-decoration: none; color: white; font-weight: 700;"  href="market.php">Normal</a> </p>
+            </div>
+            <div>
+                <h4>Knowledgr Base</h4>
+                <p>Lorem ipsum </p>
+                <p>Lorem ipsum </p>
+                <p>Lorem ipsum </p>
+                <p>Lorem ipsum </p>
+                <p>Lorem ipsum </p>
+            </div>
+            <div>
+                <h4>Useful Links</h4>
+                <p><a style=" text-decoration: none; color: white; font-weight: 700;"  href="index.php">Home</a> </p>
+                <p><a style=" text-decoration: none; color: white; font-weight: 700;"  href="market.php">Market</a> </p>
+                <p><a style=" text-decoration: none; color: white; font-weight: 700;"  href="cart.php">Cart</a> </p>
+                <p><a style=" text-decoration: none; color: white; font-weight: 700;"  href="profile.php">Profile</a> </p>
+                <p><a style=" text-decoration: none; color: white; font-weight: 700;"  href="logout.php">Logout</a> </p>
+            </div>
+            <div>
+                <h4>Contact Us</h4>
+                <div class="details-flex">
+                    <i class="fa-solid fa-phone"></i>
+                    <p>+23481 3349 9101</p>
+                </div>
+                <div class="details-flex">
+                    <i class="fa-solid fa-envelope"></i>
+                    <p>info@gmail.com</p>
+                </div>
+                <div class="details-flex">
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-google-plus-g"></i>
+                    <i class="fa-solid fa-apple-whole"></i>
+                </div>
+            </div>
+        </div>
+        <hr style="color: gray;">
+        <div class="container p-0" id="footer-note">
+            <p>Designed by oluwaCyna</p>
+            <div id="payment-icon">
+                <i class="fa-brands fa-cc-mastercard"></i>
+                <i class="fa-brands fa-cc-visa"></i>
+                <i class="fa-brands fa-paypal"></i>
+            </div>
+        </div>
+    </footer>
+    </div>
+    <script src="https://kit.fontawesome.com/b41ff46bee.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ Food_Delivery-AppColab/navigation.php
@@ -0,0 +1,16 @@
+<div class="" id="header">
+    <div class="" id="logo">
+        <img src="https://i.ibb.co/gTBPJ48/logo.jpg" height="100" width="150" alt="logo" />
+    </div>
+    <nav class="" id="nav">
+        <ul>
+            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="market.php">Market</a></li>
+            <li class="nav-item"><a class="nav-link" href="cart.php">Cart</a></li>
+            <li class="nav-item"><a class="nav-link" href="profile.php">Profile</a></li>
+            <li class="nav-item"><a class="nav-link" href="regisration.php">Register</a></li>
+            <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+            <!-- <li class="nav-item"><a class="nav-link" href="logout.php">logout</a></li> -->
+        </ul>
+    </nav>
+</div>
